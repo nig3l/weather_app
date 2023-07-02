f622f79d8c111f117129bbbfeaf5ebd9845fb06d
nigel <nigelchimwene@gmail.com>
2023-07-02 15:57:13 +0300

    views

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -3,5 +3,24 @@ import requests
 
 # Create your views here.
 
-def index(requests):
-    return render (requests,'weather/index.html')
+def index(request):
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
+
+
+    city = 'Las Vegas'
+
+    api_key = ''
+
+    city_weather = requests.get(url.format(city, api_key)).json()
+
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather':weather}
+
+    return render (request,'weather/index.html',context)
